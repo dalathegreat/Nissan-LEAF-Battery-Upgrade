dfb74aab5b876dd95d2508f7120d28c7fd3b84ca
Christoph Schleifenbaum <christoph.schleifenbaum@kdab.com>
2023-11-08 18:48:02 +0100

    Cleanup the repository for 3port
    - Remove everything created by the compilation process
    - Put it into .gitignore

--- Software/CANBRIDGE-3port/leaf-can-bridge-3-port/Debug/LUFA/Drivers/USB/Core/XMEGA/Template/Template_Endpoint_Control_W.d
+++
@@ -1,3 +0,0 @@
-LUFA/Drivers/USB/Core/XMEGA/Template/Template_Endpoint_Control_W.d \
- LUFA/Drivers/USB/Core/XMEGA/Template/Template_Endpoint_Control_W.o: \
- ../LUFA/Drivers/USB/Core/XMEGA/Template/Template_Endpoint_Control_W.c
--- Software/CANBRIDGE-3port/leaf-can-bridge-3-port/Debug/LUFA/Drivers/USB/Core/XMEGA/Template/Template_Endpoint_RW.d
+++
@@ -1,3 +0,0 @@
-LUFA/Drivers/USB/Core/XMEGA/Template/Template_Endpoint_RW.d \
- LUFA/Drivers/USB/Core/XMEGA/Template/Template_Endpoint_RW.o: \
- ../LUFA/Drivers/USB/Core/XMEGA/Template/Template_Endpoint_RW.c
